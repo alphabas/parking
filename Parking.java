fe46aa95c79e5ae8e050d98f5f01bdd6d4c419af
alphabas <alphabas22@gmail.com>
2024-09-20 12:59:18 +0100

    wip

--- Parking.java
+++ Parking.java
@@ -11,7 +11,11 @@ public class Parking {
 
     public static void main(String[] args) {
         processVehicles();
-
+        /*
+         * int a = 10;
+         * int b = a;
+         * System.out.println("La valeur de A : " + a + " et de B est : " + b);
+         */
     }
 
     private static void processVehicles() {
@@ -21,52 +25,9 @@ public class Parking {
         Vehicle vehicle = new Vehicle();
         vehicle.setRegistrationNumber("LS-458-4P");
         Ticket VehicleTicket = parkingVehicleService.processIncomingVehicule(vehicle);
-        tickets.add(VehicleTicket);
         System.out.println(VehicleTicket);
-
-        Vehicle bicycle = new Bicycle();
-        bicycle.setRegistrationNumber("F0-459-R9");
-        Ticket bicycletTicket = parkingVehicleService.processIncomingVehicule(bicycle);
-        tickets.add(bicycletTicket);
-        System.out.println(bicycletTicket);
-
-        Vehicle car = new Car();
-        car.setRegistrationNumber("JDI-34-SOS");
-        Ticket carTicket = parkingVehicleService.processIncomingVehicule(car);
-        tickets.add(carTicket);
-        System.out.println(carTicket);
-
-        /*
-         * Ticket[] tickets = new Ticket[3];
-         * tickets[0] = VehicleTicket;
-         * tickets[1] = bicycletTicket;
-         * tickets[2] = carTicket;
-         */
-
-        System.out.println("Traitement des sorties par lot");
-
-        Map<Integer, Ticket> exitOrder = new HashMap<>();
-        int position = 0;
-
-        for (Ticket ticket : tickets) {
-            exitOrder.put(position, ticket);
-            position++;
-            System.out.println(parkingVehicleService.processExistingVehicle(ticket));
-
-        }
-
-        /*
-         * for (Ticket ticket : tickets) {
-         * System.out.println(parkingVehicleService.processExistingVehicle(ticket));
-         * }
-         */
-
-        Set<Integer> keySet = exitOrder.keySet();
-        for (int key : keySet) {
-            System.out
-                    .println("Position dans la liste : " + key + " : " + exitOrder.get(key).getVehicle().getCategory());
-
-        }
+        parkingVehicleService.processExistingVehicle(VehicleTicket);
+        System.out.println("Other : " + VehicleTicket);
 
     }
 
